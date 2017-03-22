9a1662c7deab220692610f30941ba8e4dd1b6349
dbzerjav <dbzerjav@gmail.com>
2017-03-22 19:55:13 +0000

    Hide links

---
+++ spec/features/users/hiding_signin_signup_links_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Hiding signin and signout links when approiate" do
+  before do
+    @john = User.create!(email: "john@example.com", password: "password" )
+  end
+  
+  scenario "while signed in" do
+    visit "/"
+    
+    click_link "Sign in"
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
+    
+    expect(page).not_to have_link("Sign in")
+    expect(page).not_to have_link("Sign up")
+    expect(page).to have_content("Sign out")
+  end
+  
+  scenario "while not signed in" do
+    visit "/"
+    
+    expect(page).to have_link("Sign in")
+    expect(page).to have_link("Sign up")
+    expect(page).not_to have_link("Sign out")
+  end
+end
