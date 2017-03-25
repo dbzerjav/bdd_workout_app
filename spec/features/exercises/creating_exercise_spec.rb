c1e3610ed2669d00f68670b0b4f21a4fc298c49b
dbzerjav <dbzerjav@gmail.com>
2017-03-25 01:43:09 +0000

    Create exercise validations

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -5,14 +5,14 @@ RSpec.feature "Creating exercise" do
   before do
     @john = User.create!(email: "john@example.com", password: "password" )
     login_as(@john)
-  end
-  
-  scenario "with valid inputs" do
+        
     visit "/"
-    
     click_link "My Lounge"
     click_link "New Workout"
     expect(page).to have_link("Back")
+  end
+  
+  scenario "with valid inputs" do
     
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
@@ -27,12 +27,7 @@ RSpec.feature "Creating exercise" do
   end
   
   scenario "with invaild inputs" do
-    
-    visit "/"
-    click_link "My Lounge"
-    click_link "New Workout"
-    expect(page).to have_link("Back")
-    
+
     fill_in "Duration", with: ""
     fill_in "Workout details", with: ""
     fill_in "Activity date", with: ""
