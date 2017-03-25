dede20bbc2831eb97693a78b0767686c57afa161
dbzerjav <dbzerjav@gmail.com>
2017-03-25 03:39:50 +0000

    write editing exercise feature spec

---
+++ spec/features/exercises/editing_exercise_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.feature "Editing an exercise" do
+  
+  before do
+    @john = User.create(email: "john@example.com", password: "password" )
+    
+    @john_exercise = @john.exercises.create!(duration_in_min: 20,
+                                  workout: "My body building activity",
+                                  workout_date: Date.today)
+    login_as(@john)
+  end
+  
+  scenario "with valid data succeeds" do
+    visit "/"
+    
+    click_link "My Lounge"
+    
+    path = "/users/#{@john.id}/exercises/#{@john_exercise.id}/edit"
+    link = "a[href=\'#{path}\']"
+    find(link).click 
+    
+    fill_in "Duration", with: 70
+    click_button "Update Exercise"
+    
+    
+    expect(page).to have_content("Exercise has been updated")
+    expect(page).to have_content(70)
+    expect(page).not_to have_content(20)
+    
+  end
+  
+    scenario "A user fails to update an article" do
+    visit "/"
+    
+    click_link "My Lounge"
+    
+    path = "/users/#{@john.id}/exercises/#{@john_exercise.id}/edit"
+    link = "a[href=\'#{path}\]"
+    find(link).click
+    
+    fill_in "Duration", with: ""
+    click_button "Update Exercise"
+    
+    expect(page).to have_content("Exercise has not been updated")
+
+  end
+  
+end
