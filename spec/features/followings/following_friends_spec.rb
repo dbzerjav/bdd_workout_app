03ad59b9e817394ecfebdde4176e621929314e29
dbzerjav <dbzerjav@gmail.com>
2017-03-26 17:31:08 +0000

    Write following users spec

---
+++ spec/features/followings/following_friends_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Folloing friends" do
+  before do
+    @john = User.create(first_name: "John", 
+                        last_name: "Doe", 
+                        email: "john@example.com", 
+                        password: "password" )
+                        
+    @jim = User.create( first_name: "Jim", 
+                        last_name: "Doe", 
+                        email: "jim@example.com", 
+                        password: "password" )
+    login_as(@john)
+    
+  end
+    
+    scenario "if signed in" do
+      visit "/"
+      
+      expect(page).to have_content(@john.full_name)
+      expect(page).to have_content(@jim.full_name)
+      
+      href = "/friendships?friend_id=#{@john.id}"
+      expect(page).not_to have_link("Follow", :href => href)
+      
+      link = "a[href='/friendships?friend_id#{@jim.id}']"
+      find(link).click
+      
+      href = "/friendships?friend_id=#{@jim.id}"
+      expect(page).not_to have_link("Follow")
+    end
+  
+end
